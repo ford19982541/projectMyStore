31fd01200d0d4bc5db346e1e2b5676b58142137a
shiss <ford19982541@gmail.com>
2018-02-27 23:44:15 +0700

    edit urls.py #urlpatterns account > magashoes

--- projectMyStore/urls.py
+++ projectMyStore/urls.py
@@ -18,5 +18,5 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^account/',include('accounts.urls')),
+    url(r'^magashoes/',include('accounts.urls')),
 ]
