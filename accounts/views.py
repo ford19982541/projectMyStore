daa0173a971ac79234c3806d9ad72a6414d21dc3
shiss <ford19982541@gmail.com>
2018-03-08 21:13:31 +0700

    blog post admin CRUD

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'accounts/home.html')
+def contact(request):
+    return render(request,'accounts/basic.html',{'content':['if you world like to contact ,please email ','ford@gmail.com']})
--- projectMyStore/settings.py
+++ projectMyStore/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 
     'accounts',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
